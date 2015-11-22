e004b38d3498f13c8eb2ba22169741f4c4b8ae04
KarthikDinakaran <kdinakar@gmail.com>
2015-11-22 12:05:03 -0800

    Assignment Result
    Implemented the makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are functions that would create a special wrapper around the matrix data
+## structure and include a caching logic as part of the wrapper code.
+## 
 
-## Write a short comment describing this function
+## This method takes an existing matrix object as argument and returns a wrapper 
+## around the matrix object. The wrapper is essentially a list of "handlers" that 
+## can be used to access the matrix object inside it, and also access the cached 
+## inverse. This interface/handlers are used by the cacheSolve function below to 
+## calculate inverse and also update the cached value in it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(solve) s <<- solve
+        getSolve <- function() s
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## This function takes the matrix wrapper created by the makeCacheMatrix function 
+## and returns the cached value of inverse if it exist, and if there is no cached
+## value, it will calculate the inverse, update the cache as well as return it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        slv <- x$getSolve()
+        if(!is.null(slv)) {
+                message("getting cached data")
+                return(slv)
+        }
+        data <- x$get()
+        slv <- solve(data, ...)
+        x$setSolve(slv)
+        slv
 }
